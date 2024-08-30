450bf4540b168a42da2e30587cdcc24e414c9e7d
Alexander Vasyliev <saddon11@gmail.com>
2024-08-30 22:41:57 +0200

    done: prep ex week 1

--- Week1/prep-exercises/1-catwalk-promises/index.js
+++ Week1/prep-exercises/1-catwalk-promises/index.js
@@ -7,20 +7,29 @@ const DANCING_CAT_URL =
   'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
 
 function walk(img, startPos, stopPos) {
-  return new Promise((resolve) => {
-    // Resolve this promise when the cat (`img`) has walked from `startPos` to
-    // `stopPos`.
-    // Make good use of the `STEP_INTERVAL_PX` and `STEP_INTERVAL_MS`
-    // constants.
+  return new Promise(resolve => {
+    let currentPos = startPos;
+    img.style.left = currentPos + img.width;
+
+    const interval = setInterval(() => {
+      if (currentPos < stopPos - img.width) {
+        currentPos += STEP_SIZE_PX;
+        img.style.left = parseInt(img.style.left, 10) + STEP_SIZE_PX + 'px';
+      } else {
+        clearInterval(interval);
+        resolve();
+      }
+    }, STEP_INTERVAL_MS);
   });
 }
 
 function dance(img) {
-  return new Promise((resolve) => {
-    // Switch the `.src` of the `img` from the walking cat to the dancing cat
-    // and, after a timeout, reset the `img` back to the walking cat. Then
-    // resolve the promise.
-    // Make good use of the `DANCING_CAT_URL` and `DANCE_TIME_MS` constants.
+  return new Promise(resolve => {
+    img.src = DANCING_CAT_URL;
+    setTimeout(() => {
+      img.src = 'http://www.anniemation.com/clip_art/images/cat-walk.gif';
+      resolve();
+    }, DANCE_TIME_MS);
   });
 }
 
@@ -30,11 +39,19 @@ function catWalk() {
   const centerPos = (window.innerWidth - img.width) / 2;
   const stopPos = window.innerWidth;
 
-  // Use the `walk()` and `dance()` functions to let the cat do the following:
-  // 1. Walk from `startPos` to `centerPos`.
-  // 2. Then dance for 5 secs.
-  // 3. Then walk from `centerPos` to `stopPos`.
-  // 4. Repeat the first three steps indefinitely.
+  walk(img, startPos, centerPos)
+    .then(() => {
+      return dance(img);
+    })
+    .then(() => {
+      return walk(img, centerPos, stopPos);
+    })
+    .then(() => {
+      catWalk();
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
 
 window.addEventListener('load', catWalk);
